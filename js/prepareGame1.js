b81e84811c1f2ab8d878416b454de12762f23e84
Muata Nkosi <mnkosi@fandm.edu>
2018-05-05 23:48:16 -0400

    figured out player hand structue

--- js/prepareGame1.js
+++ js/prepareGame1.js
@@ -1,24 +1,31 @@
 var deck1 = Deck();
     $container = document.getElementById('container');
+    playerContainer = document.getElementById('playerhand');
 deck1.mount($container);
-//deck1.shuffle();
+
 
 //$(document.ready(function()
 //))
 
 //could use this for index page 
-deck1.cards.forEach(function (card, i) {
-    card.setSide(Math.random() < 0.5 ? 'front' : 'back');
-    card.enableFlipping();
-    card.enableDragging();
+// deck1.cards.forEach(function (card, i) {
+//     card.setSide(Math.random() < 0.5 ? 'front' : 'back');
+//     card.enableFlipping();
+//     card.enableDragging();
 
-    // explode
-    card.animateTo({
-        delay: 1000 + i * 2, // wait 1 second + i * 2 ms
-        duration: 500,
-        ease: 'quartOut',
+//     // explode
+//     card.animateTo({
+//         delay: 1000 + i * 2, // wait 1 second + i * 2 msz
+//         duration: 500,
+//         ease: 'quartOut',
 
-        x: Math.random() * window.innerWidth - window.innerWidth / 2,
-        y: Math.random() * window.innerHeight - window.innerHeight / 2
-    });
-});
+//         x: Math.random() * window.innerWidth - window.innerWidth / 2,
+//         y: Math.random() * window.innerHeight - window.innerHeight / 2
+//     });
+// });
+var firstCard = deck1.cards[0];
+deck1.shuffle();
+
+firstCard.mount(playerContainer);
+
+firstCard.enableFlipping();

a0c9831298d472595cab691fbbb31797498b56cf
Muata Nkosi <mnkosi@fandm.edu>
2018-08-20 22:02:59 -0400

    added test for end game

--- test/blackjack.test.js
+++ test/blackjack.test.js
@@ -40,7 +40,9 @@ describe('Start Game', function() {
     })
 
     it('should test end game conditions.', function() {
-        assert.equal(true,false,'Test this by using combo of gameOver, playerWin, etc.');
+        endGame('')
+        assert.equal($('#new-game-button').is(':visible'),true,"New Game button is not visible when it should be.");
+        assert.equal($('#stay-button').is(':hidden'),true,"Stay button is not hidden when it should be.");
     })
 })
 
@@ -48,7 +50,7 @@ describe('Start Game', function() {
 describe('UpdateGameButtons', function (){
     it(' should change on screen buttons once game starts.', function(){
         updateGameButtons(true);
-        assert.equal($('#new-game-button').is(':hidden'),true,"New Game button is visible when it shouldn't be.");
+        assert.equal($('#new-game-button').is(':hidden'),true,"New Game button is not hidden when it should be.");
         assert.equal($('#stay-button').is(':visible'),true,"Stay button is not visible when it should be.");
     });
 

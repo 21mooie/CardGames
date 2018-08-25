73a27142208d8c5acdb7272963cc49297832d84e
Muata Nkosi <mnkosi@fandm.edu>
2018-08-25 17:28:56 -0400

    installed webpack, moved codeto dist folder

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ app.use("/css", express.static(__dirname));
 app.use("/js", express.static(__dirname + '/js'));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + 'index.html'));
+    res.sendFile(path.join(__dirname + '/dist/' + 'index.html'));
 });
 
 app.get('/blackjack.html', function (req, res) {
-    res.sendFile(path.join(__dirname + 'blackjack.html'));
+    res.sendFile(path.join(__dirname + '/dist/' + 'blackjack.html'));
 });
 
 app.listen(process.env.PORT || 8080, function(){

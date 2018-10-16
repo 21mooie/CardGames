45d8dbef455113f929a1a1398d939367d2249a77
Muata Nkosi <mnkosi@fandm.edu>
2018-10-15 23:18:46 -0400

    Implemented path traversal protection against attackers

--- server.js
+++ server.js
@@ -2,26 +2,30 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname + '/dist/' )));
 // app.use("/css", express.static(__dirname));
-app.use('/js', express.static(__dirname + '/dist/'));
+// app.use('/js', express.static(__dirname + '/dist/'));
 // app.use('/dist/bundles',express.static(__dirname + '/dist/bundles'))
 
-app.get('/', function (req, res) {
-    res.redirect('/dist');
-});
+// app.get('/', function (req, res) {
+//     res.redirect('/dist');
+// });
 
-app.get('/dist', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/' + 'index.html'));
-});
+// app.get('/dist', function (req, res) {
+//     res.sendFile(path.join(__dirname + 'index.html'));
+// });
 
 app.get('/blackjack.html', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/' + 'blackjack.html'));
+    res.sendFile(path.join(__dirname  + 'blackjack.html'));
 });
 
 app.get('/index.html', function (req,res){
     res.redirect('/');
-})
+});
+
+app.get('*', function(req,res){
+    res.redirect('/');
+});
 
 app.listen(process.env.PORT || 8080, function(){
     console.log('The server is listening on port 8080.');
